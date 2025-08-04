0d2ed832835a57a3f440a36ab065cc5993f8383b
Inferno Services <asher.infernoservices@gmail.com>
2025-08-04 15:42:00 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "mind-bloom-vdm4y",
-  "appId": "1:387530632714:web:f2f49dc8454a9a3fa8b9f3",
-  "storageBucket": "mind-bloom-vdm4y.firebasestorage.app",
-  "apiKey": "AIzaSyD5lES72nwynLNFdaYog6lxVxrw_Lm72pI",
-  "authDomain": "mind-bloom-vdm4y.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "387530632714"
+  apiKey: "AIzaSyD5lES72nwynLNFdaYog6lxVxrw_Lm72pI",
+  authDomain: "mind-bloom-vdm4y.firebaseapp.com",
+  projectId: "mind-bloom-vdm4y",
+  storageBucket: "mind-bloom-vdm4y.firebasestorage.app",
+  messagingSenderId: "387530632714",
+  appId: "1:387530632714:web:f2f49dc8454a9a3fa8b9f3"
 };
 
 
